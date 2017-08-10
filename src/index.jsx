793855fc8f533bd9a8ada3adb2ca4e9dd104d2ae
Mara Timberlake <maratimberlake@msn.com>
2017-08-10 15:00:02 -0700

    alter index.html and replace index.js with index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 ReactDOM.render(
   <div>
     <h1>Help Queue</h1>
@@ -5,7 +8,6 @@ ReactDOM.render(
     <h3>3a</h3>
     <h3>Thato and Haley</h3>
     <h3>4 mins</h3>
-    <p><em>Firebase won't save record</em></p>
   </div>,
   document.getElementById('react-app-root')
 );
